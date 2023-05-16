d1bf4d73fe647bb285594fb7392f6803356bf726
VivaLaBaguette <96511156+VivaLaBaguette@users.noreply.github.com>
2023-05-16 11:50:52 -0700

    adding code for bot movement and bot sensor modules

--- Final_Code/bot_Movement.h
+++ Final_Code/bot_Movement.h
@@ -1,21 +1,33 @@
 /*file for movement of the robot
 */
 
-#ifndef BOT_H
-#define BOT_H
+#ifndef BOT_MOVE_H
+#define BOT_MOVE_H
 
 #include <BOARD.h>
 
 #define BUMPER_TRIPPED 1
 #define BUMPER_NOT_TRIPPED 0
 
-void Bot_Init(void);
+//init bot
+void Bot_Move_Init(void);
 
-void Bot_Left_Speed(char newSpeed);
+//control left motor speed
+void Bot_Left_Motor(char newSpeed);
 
-void Bot_Right_Speed(char newSpeed);
+//control right motor speed
+void Bot_Right_Motor(char newSpeed);
 
-unsigned int Bot_Battery(void);
+//control flywheel motor
+void Bot_FlyWheel(char newSpeed);
 
-unsigned char Bot_Bumpers(void);
+//control Load motor 	
+void Bot_Loader(char newSpeed);
 
+//set leds
+char Bot_LEDSSET(uint16_t pttern);
+
+//number to light 0-12 leds
+char Bot_Bar_Graph(uint8_t Number);
+
+#endif
