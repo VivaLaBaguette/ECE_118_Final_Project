96885f78fc724e4ff1908e443e427c7482d5ef72
VivaLaBaguette <96511156+VivaLaBaguette@users.noreply.github.com>
2023-05-16 10:55:43 -0700

    setting up bot header

--- Final_Code/bot_Movement.h
+++ Final_Code/bot_Movement.h
@@ -1,4 +1,21 @@
+/*file for movement of the robot
+*/
+
 #ifndef BOT_H
 #define BOT_H
 
 #include <BOARD.h>
+
+#define BUMPER_TRIPPED 1
+#define BUMPER_NOT_TRIPPED 0
+
+void Bot_Init(void);
+
+void Bot_Left_Speed(char newSpeed);
+
+void Bot_Right_Speed(char newSpeed);
+
+unsigned int Bot_Battery(void);
+
+unsigned char Bot_Bumpers(void);
+
